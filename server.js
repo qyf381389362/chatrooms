8d522d70546add7b697b3b14cae39e1556e2ec67
qyf381389362 <381389362@qq.com>
2018-05-08 15:54:35 +0800

    创建项目目录，添加依赖项

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
-let http = require('http')
-let fs = require('fs')
-let path = require('path') // 内置的path模块提供了与文件系统路径相关的功能
-let mime = require('mime') // 附加的mime模块有根据文件扩展名得出mime类型的能力
-let cache = {}
+let http = require('http');
+let fs = require('fs');
+let path = require('path'); // 内置的path模块提供了与文件系统路径相关的功能
+let mime = require('mime'); // 附加的mime模块有根据文件扩展名得出mime类型的能力
+let cache = {};
+
+function send404 (response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: resource not found');
+  response.end();
+}
+
+function sendFile (response, filePath, fileContents) {
+  response.writeHead(
+    200,
+    {'Content-type': mime.lookup(path.basename(filePath))}
+  );
+  response.end(fileContents);
+}
+
+function serveStatic (response, cache, absPath) {
+  if (cache[absPath]) { // 检查文件是否缓存在内存里
+    sendFile(response, absPath, cache[absPath]);
+  } else {
+    fs.exists(absPath, function (exists) { // 检查文件是否存在
+      if(exists) {
+
+      }
+    })
+  }
+}
