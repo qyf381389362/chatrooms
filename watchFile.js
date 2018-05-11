d843242d355af9f7ecef424d889dca4f44ff9c3b
qyf381389362 <381389362@qq.com>
2018-05-11 21:15:06 +0800

    加了一些其他的测试代码，和聊天程序无关

---
+++ watchFile.js
@@ -0,0 +1,34 @@
+var events = require('events');
+var util = require('util');
+var fs = require('fs');
+var watchDir = './watch', processedDir = './done';
+function Watcher (watchDir, processedDir) {
+  this.watchDir = watchDir;
+  this.processedDir = processedDir;
+}
+util.inherits(Watcher, events.EventEmitter);
+Watcher.prototype.watch = function () {
+  var watcher = this;
+  fs.readdir(this.watchDir, function (err, files) {
+    if (err) throw err;
+    for (var index in files) {
+      watcher.emit('process', files[index]);
+    }
+  });
+}
+Watcher.prototype.start = function () {
+  var watcher = this;
+  fs.watchFile(watchDir, function () {
+    watcher.watch();
+  });
+}
+
+var watcher = new Watcher(watchDir, processedDir);
+watcher.on('process', function process (file) {
+  var watchFile = this.watchDir + '/' + file;
+  var processedFile = this.processedDir + '/' + file.toLowerCase();
+  fs.rename(watchFile, processedFile, function (err) {
+    if (err) throw err;
+  });
+});
+watcher.start();
